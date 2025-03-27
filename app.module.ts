c7c2ebd000147976b6c0eb240d286433c9f8cae4
Shanavas <shanavascet04@gmail.com>
2025-03-27 14:18:04 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { Web1QueryBuilderComponent } from './components/web1-query-builder/web1-query-builder.component';
+import { CampaignSegmentationComponent } from './components/campaign-segmentation/campaign-segmentation.component';
+import { LoanExclusionComponent } from './components/loan-exclusion/loan-exclusion.component';
+import { ReleaseNotesComponent } from './components/release-notes/release-notes.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    Web1QueryBuilderComponent,
+    CampaignSegmentationComponent,
+    LoanExclusionComponent,
+    ReleaseNotesComponent,
+    ContactUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
